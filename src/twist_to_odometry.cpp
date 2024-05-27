98454ebb29d179e7c994926ff36dbf203643bf6b
yus-ko <koike3691@gmail.com>
2024-05-27 20:24:33 +0900

    fix megarover21 act

--- src/twist_to_odometry.cpp
+++ src/twist_to_odometry.cpp
@@ -58,6 +58,23 @@ void dead_reckoning(geometry_msgs::TwistStamped vel_msg)
 
 }
 
+void odom_broadcast(nav_msgs::Odometry odom)
+{
+	static tf2_ros::TransformBroadcaster dynamic_br;
+	geometry_msgs::TransformStamped transformStamped;
+    transformStamped.header.stamp = odom.header.stamp;
+    transformStamped.header.frame_id = odom.header.frame_id;
+    transformStamped.child_frame_id = odom.child_frame_id;
+    transformStamped.transform.translation.x = odom.pose.pose.position.x;
+    transformStamped.transform.translation.y = odom.pose.pose.position.y;
+    transformStamped.transform.translation.z = odom.pose.pose.position.z;
+    transformStamped.transform.rotation.x = odom.pose.pose.orientation.x;
+    transformStamped.transform.rotation.y = odom.pose.pose.orientation.y;
+    transformStamped.transform.rotation.z = odom.pose.pose.orientation.z;
+    transformStamped.transform.rotation.w = odom.pose.pose.orientation.w;
+    dynamic_br.sendTransform(transformStamped);
+}
+
 void twist_callback(const geometry_msgs::Twist& msg)
 {
 	// ROS_INFO("twist callback");
@@ -68,19 +85,29 @@ void twist_callback(const geometry_msgs::Twist& msg)
 
 	dead_reckoning(tm);
 
+	odom_broadcast(g_odom);
 	g_pub_odom.publish(g_odom);
 }
 
 void pose_callback(const geometry_msgs::PoseWithCovarianceStamped& msg)
 {
-	// ROS_INFO("imu callback");
+	// ROS_INFO("pose callback");
 	g_odom.pose = msg.pose;
+}
+
+void imu_callback(const sensor_msgs::Imu& msg)
+{
+	ROS_INFO("imu callback");
+	double roll,pitch,yaw;
+	potbot_lib::utility::get_RPY(msg.orientation, roll, pitch, yaw);
+	ROS_INFO("(r,p,y) = (%f, %f, %f)", roll, pitch, yaw);
+	// g_odom.pose = msg.pose;
 
 }
 
 int main(int argc,char **argv){
 	ros::init(argc,argv,"potbot_twist_to_odometry");
-
+	
 	ros::NodeHandle n("~");
 	
 	double x=0,y=0,z=0,roll=0,pitch=0,yaw=0;
@@ -101,8 +128,9 @@ int main(int argc,char **argv){
 	ros::NodeHandle nh;
 	ros::Subscriber sub_twist					= nh.subscribe(G_TOPIC_SUB_TWIST,1,&twist_callback);
 	ros::Subscriber sub_pose					= nh.subscribe(G_TOPIC_SUB_INITPOSE,1,&pose_callback);
+	// ros::Subscriber sub_imu						= nh.subscribe("/robot_5/camera/imu/data_raw",1,&imu_callback);
 	g_pub_odom									= nh.advertise<nav_msgs::Odometry>(G_TOPIC_PUB_ODOM, 1);
-
+	
 	g_odom.pose.pose							= potbot_lib::utility::get_Pose(x,y,z,roll,pitch,yaw);
 
 	ros::spin();
